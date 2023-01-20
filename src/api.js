167b1061d1a93efeb5bd3432c0b8ad5db3bdccd9
Gabriel <gabrielmurryedh@gmail.com>
2023-01-20 15:16:42 -0800

    added cors

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const axios = require("axios").default;
+const cors = require("cors");
 
 const serverless = require("serverless-http");
 
 const app = express();
+app.use(cors());
 
 const router = express.Router();
 
@@ -12,11 +15,35 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/test", (req, res) => {
-  res.json({
-    hello: "test!",
-  });
+app.post("/solve", (req, res) => {
+  const options = {
+    method: "POST",
+    url: "https://solve-sudoku.p.rapidapi.com/",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "solve-sudoku.p.rapidapi.com",
+    },
+    data: {
+      puzzle: req.body.numbers,
+    },
+  };
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
+
+// router.get("/test", (req, res) => {
+//   res.json({
+//     hello: "test!",
+//   });
+// });
 
 app.use("/.netlify/functions/api", router);
 
