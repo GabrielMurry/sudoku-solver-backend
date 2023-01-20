772ee16d4b5dff44e5c68a8412bdcda340e4eaed
Gabriel <gabrielmurryedh@gmail.com>
2023-01-20 15:22:05 -0800

    test

--- src/api.js
+++ src/api.js
@@ -15,29 +15,29 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/solve", (req, res) => {
-  const options = {
-    method: "POST",
-    url: "https://solve-sudoku.p.rapidapi.com/",
-    headers: {
-      "content-type": "application/json",
-      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-      "X-RapidAPI-Host": "solve-sudoku.p.rapidapi.com",
-    },
-    data: {
-      puzzle: req.body.numbers,
-    },
-  };
-  axios
-    .request(options)
-    .then((response) => {
-      console.log(response.data);
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-});
+// router.post("/solve", (req, res) => {
+//   const options = {
+//     method: "POST",
+//     url: "https://solve-sudoku.p.rapidapi.com/",
+//     headers: {
+//       "content-type": "application/json",
+//       "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+//       "X-RapidAPI-Host": "solve-sudoku.p.rapidapi.com",
+//     },
+//     data: {
+//       puzzle: req.body.numbers,
+//     },
+//   };
+//   axios
+//     .request(options)
+//     .then((response) => {
+//       console.log(response.data);
+//       res.json(response.data);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// });
 
 // router.get("/test", (req, res) => {
 //   res.json({
