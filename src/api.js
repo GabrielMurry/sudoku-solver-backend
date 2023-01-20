ac9de13fec0073119ae2c6ede7e29af44358a00a
Gabriel <gabrielmurryedh@gmail.com>
2023-01-20 15:24:08 -0800

    test 2

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const axios = require("axios").default;
-const cors = require("cors");
+// const cors = require("cors");
 
 const serverless = require("serverless-http");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 const router = express.Router();
 
