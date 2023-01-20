7a0e0fe463c979806d682763275bfe405c0e8a28
Gabriel <gabrielmurryedh@gmail.com>
2023-01-20 15:27:32 -0800

    test 4

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios").default;
 
 const serverless = require("serverless-http");
 
@@ -10,6 +11,30 @@ router.get("/", (req, res) => {
   res.json({
     hello: "hi!",
   });
+});
+
+router.post("/solve", (req, res) => {
+  const options = {
+    method: "POST",
+    url: "https://solve-sudoku.p.rapidapi.com/",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "solve-sudoku.p.rapidapi.com",
+    },
+    data: {
+      puzzle: req.body.numbers,
+    },
+  };
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 // router.get("/test", (req, res) => {
